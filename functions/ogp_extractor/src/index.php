87eede7ba12351f3d85de260d46c4b0f0414f047
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-05-27 10:05:34 +0530

    update template

--- functions/ogp_extractor/src/index.php
+++ functions/ogp_extractor/src/index.php
@@ -1,58 +1,39 @@
 <?php
 
-use Appwrite\Client;
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-// You can remove imports of services you don't use
-use Appwrite\Services\Account;
-use Appwrite\Services\Avatars;
-use Appwrite\Services\Databases;
-use Appwrite\Services\Functions;
-use Appwrite\Services\Health;
-use Appwrite\Services\Locale;
-use Appwrite\Services\Storage;
-use Appwrite\Services\Teams;
-use Appwrite\Services\Users;
+// use Appwrite\Client;
+// use Appwrite\Exception;
 
-require_once 'vendor/autoload.php';
+// This is your Appwrite function
+// It's executed each time we get a request
+return function ($context) {
+    // Why not try the Appwrite SDK?
+    //
+    // $client = new Client();
+    // $client
+    //     ->setEndpoint('https://cloud.appwrite.io/v1')
+    //     ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
+    //      ->setKey(getenv('APPWRITE_API_KEY'));
 
-/*
-  '$req' variable has:
-    'headers' - object with request headers
-    'payload' - request body data as a string
-    'variables' - object with function variables
+    // You can log messages to the console
+    $context->log('Hello, Logs!');
 
-  '$res' variable has:
-    'send(text, status)' - function to return text response. Status code defaults to 200
-    'json(obj, status)' - function to return JSON response. Status code defaults to 200
+    // If something goes wrong, log an error
+    $context->error('Hello, Errors!');
 
-  If an error is thrown, a response with code 500 will be returned.
-*/
+    // The `req` object contains the request data
+    if ($context->req->method === 'GET') {
+        // Send a response with the res object helpers
+        // `res.send()` dispatches a string back to the client
+        return $context->res->send('Hello, World!');
+    }
 
-return function($req, $res) {
-  $client = new Client();
-
-  // You can remove services you don't use
-  $account = new Account($client);
-  $avatars = new Avatars($client);
-  $database = new Databases($client);
-  $functions = new Functions($client);
-  $health = new Health($client);
-  $locale = new Locale($client);
-  $storage = new Storage($client);
-  $teams = new Teams($client);
-  $users = new Users($client);
-
-  if(!$req['variables']['APPWRITE_FUNCTION_ENDPOINT'] || !$req['variables']['APPWRITE_FUNCTION_API_KEY']) {
-    echo('Environment variables are not set. Function cannot use Appwrite SDK.');
-  } else {
-    $client
-      ->setEndpoint($req['variables']['APPWRITE_FUNCTION_ENDPOINT'])
-      ->setProject($req['variables']['APPWRITE_FUNCTION_PROJECT_ID'])
-      ->setKey($req['variables']['APPWRITE_FUNCTION_API_KEY'])
-      ->setSelfSigned(true);
-  }
-
-  $res->json([
-    'areDevelopersAwesome' => true
-  ]);
+    // `res.json()` is a handy helper for sending JSON
+    return $context->res->json([
+        'motto' => 'Build like a team of hundreds_',
+        'learn' => 'https://appwrite.io/docs',
+        'connect' => 'https://appwrite.io/discord',
+        'getInspired' => 'https://builtwith.appwrite.io',
+    ]);
 };
