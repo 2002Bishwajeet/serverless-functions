8314fa43003a22702da1ca7a12f38f650411d54c
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-05-28 15:36:33 +0530

    enable curl

--- functions/ogp_extractor/src/Ogp.php
+++ functions/ogp_extractor/src/Ogp.php
@@ -8,16 +8,14 @@ class Ogp
     public static function getData(string $url): array
     {
         try {
-            // No need for this . I made a mistake somewhere else. The url needs to be decoded.
-            // $ch = curl_init($url);
-            // $headers = [
-            //     "Content-Type" => "text/html",
-            // ];
-            // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            // curl_setopt($ch, CURLOPT_HTTPGET, true);
-            // $content = curl_exec($ch);
-            $content = file_get_contents($url);
+             $ch = curl_init($url);
+             $headers = [
+                 "Content-Type" => "text/html",
+             ];
+             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+             curl_setopt($ch, CURLOPT_HTTPGET, true);
+             $content = curl_exec($ch);
             if (!$content) {
                 throw new \Exception('Could not fetch content from url' . $content);
             }
