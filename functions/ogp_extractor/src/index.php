f9bc606468b61534d40ffe9782df88ddbed4607d
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-05-28 15:38:07 +0530

    fix url decode

--- functions/ogp_extractor/src/index.php
+++ functions/ogp_extractor/src/index.php
@@ -1,39 +1,35 @@
 <?php
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/autoload.php');
 
 use OGP\Ogp;
 
-
+// This is your Appwrite function
+// It's executed each time we get a request
 return function ($context) {
-    // the url must have the query string
-    // if the query isn't present, return with an error
     try {
-        if(!isset($context->req->query['url'])){
+        if (!isset($context->req->query['url'])) {
             $context->error("No url query parameter");
             return $context->res->json(["error" => "url query parameter is required"]);
         }
 
-        $url = $context->req->query['url'];
-
-
+        $url = urldecode($context->req->query['url']);
+        $context->log("This is the url" . $url);
 
         // If the request method is GET, return the complete OGP data
         if ($context->req->method === 'GET') {
+
             try {
                 $ogp = Ogp::getData($url);
                 return $context->res->json($ogp);
-
-            } catch (\Exception $e) {
-                $context->error("Could not fetch content from url" . $e->getMessage());
+            } catch (\Throwable $e) {
+                $context->error($e->getMessage());
                 return $context->res->json(["error" => "Could not fetch content from url. See Developer Console for more details"]);
             }
         }
         return $context->res->json(["error" => "Invalid request method"]);
-
-    } catch (\Exception $e) {
+    } catch (\Throwable $e) {
         $context->error("An error occurred: " . $e->getMessage());
         return $context->res->json(["error" => "An error occurred. See Developer Console for more details"]);
     }
-
 };
