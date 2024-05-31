c074fe9fc493edd3642302c69c8be1c6d906ee42
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-05-31 16:57:08 +0530

    chore: add User Agent

--- functions/ogp_extractor/src/Ogp.php
+++ functions/ogp_extractor/src/Ogp.php
@@ -12,6 +12,7 @@ class Ogp
              $ch = curl_init($url);
              $headers = [
                  "Content-Type" => "text/html",
+                 "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
              ];
             curl_setopt_array($ch, [
                 CURLOPT_FOLLOWLOCATION => true,
